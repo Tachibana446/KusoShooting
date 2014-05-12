bc4b4d84eff193056b5dfc3479dbb274ae49c132
Tachibana446 <stinger33z@gmail.com>
2014-05-13 02:05:49 +0900

    イベントとラベルを使ってみた

--- script.js
+++ script.js
@@ -4,11 +4,27 @@ window.onload = function(){
 	
 	game.preload('img/title1000-1000.png');
 	game.onload = function(){
-		var scene = new Scene();
-		var sprite = new Sprite(1000,1000);
-		sprite.image = game.assets['img/title1000-1000.png'];
-		scene.addChild(sprite);
-		game.pushScene(scene);
+		// タイトル画面シーン
+		var title = new Scene();
+		// なんか次のシーン
+		var novel = new Scene();
+		
+		// タイトル画面のスプライト
+		var titleSprite = new Sprite(1000,1000);
+		titleSprite.image = game.assets['img/title1000-1000.png'];
+		title.addChild(titleSprite);
+		// タイトル画面のスプライトのイベント
+		titleSprite.addEventListener('touchend',function(){
+			game.popScene();
+			game.pushScene(novel);
+		});
+		
+		// なんかラベル
+		var label = new Label("超駅再ティんな３Dアクションクソゲー");
+		label.x = 50; label.y = 500; label.height = 900; label.width = 900; label.font = "48px meiryo";
+		novel.addChild(label);
+		// ゲームの開始
+		game.pushScene(title);
 		
 	};
 	game.start();
